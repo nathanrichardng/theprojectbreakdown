e4fa5c18d29d918078365bdc089b89b19ebd5391
Nathan <nathanrichardng@gmail.com>
2015-10-23 02:58:01 -1000

    updated task.owner to display user name instead of id

--- Task/TaskRoute.js
+++ Task/TaskRoute.js
@@ -5,8 +5,11 @@ Router.route('/Tasks/:TaskId', {
 		return Tasks.findOne({ _id:taskId }, {
 	        transform: function(doc) {
 	          var newDoc = doc;
+	          var owner = Meteor.users.findOne({ _id: doc.owner });
+
 	          newDoc.dueDate = newDoc.dueDate.toDateString();
-	          console.log("doc", doc, "newdoc", newDoc);
+	          newDoc.owner = owner.profile.firstName + " " + owner.profile.lastName;
+	          
 	          return newDoc;
 	        }
 	    });
